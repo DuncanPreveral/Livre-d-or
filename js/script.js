ee35edfe5e92ceac348506f6c7d37b694d43c3c2
Duncan Prévéral <44778998+DuncanPreveral@users.noreply.github.com>
2019-09-17 00:06:17 +0200

    Avancement livre d'or
    Réalisation de la page "pseudo + message"

---
+++ js/script.js
@@ -0,0 +1,9 @@
+button.addEventListener("click", getMessage);
+
+function getMessage() {
+    let mess = document.getElementById("saisie").value;
+    alert(mess);
+
+    let contain = document.createElement("div");
+    contain = 
+}
